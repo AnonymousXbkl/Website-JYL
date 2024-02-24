c7b9593df62067f5f25b9a602d0a6bea09f5abcb
AnonymousXbkl <kerjaandi01@gmail.com>
2024-02-24 18:46:37 +0700

    Penambahan animasi AOS dan footer

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-$(window).scroll(function() {
-    let wScroll = $(this).scrollTop();
-    $('.textAwalPage h3').css({
-        'transform' : 'translate(0px, '+ wScroll/4 +'%)'
-    })
+window.addEventListener ('scroll', function () {
+    const navbar = document.querySelector ('.navbar')
+        if (window.pageYOffset >= 150) {
+            navbar.classList.add ('warna')
+        } else {
+            navbar.classList.remove ('warna')
+        }
 })
