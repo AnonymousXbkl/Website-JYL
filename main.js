5cafd39b682eb342557e1d5a4f480ccb419a706d
AnonymousXbkl <kerjaandi01@gmail.com>
2024-02-26 22:21:55 +0700

    menambahkan searchButton dan functionnya

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
-
-/*
-window.addEventListener ('scroll', function () {
-    const navbar = document.querySelector ('.navbar')
-        if (window.pageYOffset >= 150) {
-            navbar.classList.add ('warna')
-        } else {
-            navbar.classList.remove ('warna')
+const searchForm = document.querySelector ('.search-form');
+const searchButton = document.getElementById ('search-button');
+const searchBox = document.querySelector ('.input-search');
+const navLink = document.querySelector ('.nav-link')
+    searchButton.addEventListener ('click', function (e) {
+        searchForm.classList.toggle ('active');
+        e.preventDefault ();
+    document.addEventListener ('click', function (e) {
+        if (!searchButton.contains(e.target) && !searchForm.contains(e.target)) {
+            searchForm.classList.remove ('active');
         }
-})
-**/
+    })
+    })
+
+
+
+
+
+
