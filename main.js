16ffbee99890be1d8df8b5b1ecc43b59e2cb19d1
AnonymousXbkl <kerjaandi01@gmail.com>
2024-02-27 08:43:51 +0700

    merapikan navbar toogler untuk icon search dan search-form

--- main.js
+++ main.js
@@ -4,13 +4,23 @@ const searchBox = document.querySelector ('.input-search');
 const navLink = document.querySelector ('.nav-link')
     searchButton.addEventListener ('click', function (e) {
         searchForm.classList.toggle ('active');
+        searchBox.focus ();
         e.preventDefault ();
     document.addEventListener ('click', function (e) {
         if (!searchButton.contains(e.target) && !searchForm.contains(e.target)) {
             searchForm.classList.remove ('active');
-        }
-    })
-    })
+        };
+    });
+    });
+
+
+const btnNavbar = document.querySelector ('.navbar-toggler');
+const navbarExtra = document.querySelector ('.navbar-ekstra');
+const navbarCollapse = document.querySelector ('.navbar-collapse')
+    btnNavbar.addEventListener ('click', function(e) {
+        searchButton.remove ();
+        searchForm.classList.toggle ('active');
+    });
 
 
 
