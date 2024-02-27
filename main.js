500258e11e2b68b4b6559be3fa25ea21cc560fdc
AnonymousXbkl <kerjaandi01@gmail.com>
2024-02-27 09:21:11 +0700

    menambahkan function untuk menutup navbar-toggler

--- main.js
+++ main.js
@@ -1,27 +1,29 @@
-const searchForm = document.querySelector ('.search-form');
-const searchButton = document.getElementById ('search-button');
-const searchBox = document.querySelector ('.input-search');
-const navLink = document.querySelector ('.nav-link')
-    searchButton.addEventListener ('click', function (e) {
-        searchForm.classList.toggle ('active');
-        searchBox.focus ();
-        e.preventDefault ();
-    document.addEventListener ('click', function (e) {
-        if (!searchButton.contains(e.target) && !searchForm.contains(e.target)) {
-            searchForm.classList.remove ('active');
-        };
-    });
-    });
+const searchForm = document.querySelector(".search-form");
+const searchButton = document.getElementById("search-button");
+const searchBox = document.querySelector(".input-search");
+const navLink = document.querySelector(".nav-link");
+searchButton.addEventListener("click", function (e) {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+  document.addEventListener("click", function (e) {
+    if (!searchButton.contains(e.target) && !searchForm.contains(e.target)) {
+      searchForm.classList.remove("active");
+    }
+  });
+});
+
+const btnNavbar = document.querySelector(".navbar-toggler");
+btnNavbar.addEventListener("click", function (e) {
+  searchButton.remove();
+  searchForm.classList.toggle("active");
+  e.stopPropagation();
+});
 
 
-const btnNavbar = document.querySelector ('.navbar-toggler');
-    btnNavbar.addEventListener ('click', function(e) {
-        searchButton.remove ();
-        searchForm.classList.toggle ('active');
-    });
+  document.addEventListener("click", function (e) {
+    if (!searchForm.contains(e.target) && !navLink.contains(e.target)) {
+      btnNavbar.click();
+    }
 
-
-
-
-
-
+});
